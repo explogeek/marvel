160b8f678a7819ffec80c6c1fd1834461fd42de9
Aleksandr Shevchuk <a.shevchuk@corp.nekki.ru>
2023-09-26 15:30:44 +0300

    update char on button, scale not found img

--- marvel/src/components/randomChar/RandomChar.js
+++ marvel/src/components/randomChar/RandomChar.js
@@ -63,7 +63,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button onClick={this.updateChar} className="button button__main">
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -75,10 +75,13 @@ class RandomChar extends Component {
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki} = char;
+    const NOT_FOUND_IMG = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg";
+
+    const imgStyle = thumbnail === NOT_FOUND_IMG? {objectFit: "contain"} : {};
 
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
